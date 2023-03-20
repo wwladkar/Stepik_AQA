770a72e4dd5475b3d473cdaecb74eb94d9f7165b
wwladkar <wwladkar@gmail.com>
2023-03-20 08:29:04 +0300

    Added mod1_les6_step10_first_assert.py

---
+++ mod1_les6_step10_first_assert.py
@@ -0,0 +1,40 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+time.sleep(1)
+
+
+try:
+
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    elements = browser.find_elements(By.CSS_SELECTOR, "div.first_block input.form-control")
+    for element in elements:
+        element.send_keys("Мой ответ")
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
