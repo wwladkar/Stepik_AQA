087105602cfcf1a52bf66355f7cd211543a620d8
wwladkar <wwladkar@gmail.com>
2023-03-22 08:01:25 +0300

    Added mod2_les1_step05_captcha_for_robots.py

---
+++ mod2_les1_step05_captcha_for_robots.py
@@ -0,0 +1,38 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import math
+
+
+def calc(a):
+    return str(math.log(abs(12 * math.sin(int(a)))))
+
+
+link = "https://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(link)
+
+    time.sleep(1)
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input_answer = browser.find_element(By.ID, "answer")
+    input_answer.send_keys(y)
+    checkbox = browser.find_element(By.ID, "robotCheckbox")
+    checkbox.click()
+    radiobutton = browser.find_element(By.ID, "robotsRule")
+    radiobutton.click()
+
+    button = browser.find_element(By.CLASS_NAME, "btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
