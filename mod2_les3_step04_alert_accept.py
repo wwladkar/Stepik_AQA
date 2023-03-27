7eac41a6cd5ceb3205fe2e142da939728daf642d
wwladkar <wwladkar@gmail.com>
2023-03-27 08:06:01 +0300

    Added mod2_les3_step04_alert_accept.py

---
+++ mod2_les3_step04_alert_accept.py
@@ -0,0 +1,40 @@
+import time
+import math
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+time.sleep(1)
+
+
+def calc(a):
+    return str(math.log(abs(12 * math.sin(int(a)))))
+
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(link)
+
+    button = browser.find_element(By.CLASS_NAME, "btn")
+    button.click()
+
+    alert = browser.switch_to.alert
+    alert.accept()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input_answer = browser.find_element(By.ID, "answer")
+    input_answer.send_keys(y)
+
+    button = browser.find_element(By.TAG_NAME, "button")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
