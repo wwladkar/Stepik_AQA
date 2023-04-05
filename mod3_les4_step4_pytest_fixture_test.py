49f82f96f359d7b7f8bb2f6983ad577188b1e6c3
wwladkar <wwladkar@gmail.com>
2023-04-05 08:22:36 +0300

    Modified mod3_les4_step4_pytest_fixture_test.py

--- mod3_les4_step4_pytest_fixture_test.py
+++ mod3_les4_step4_pytest_fixture_test.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")  # декоратор фикстуры с областью видимости "function"
 def browser():
     print("\nstart browser for test..")
     options = Options()
@@ -21,9 +21,8 @@ def browser():
 
 class TestMainPage1:
 
-    @pytest.mark.positive
+    @pytest.mark.xfail(strict=True)  # маркировка проходящего теста, как ожидаемо падающего (типа пофиксили)
     def test_registration1(self, browser):
-
         link = "http://suninjuly.github.io/registration1.html"
         browser.get(link)
 
@@ -51,10 +50,38 @@ class TestMainPage1:
         # с помощью assertEqual проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         assert "Congratulations! You have successfully registered!" == welcome_text
 
-    @pytest.mark.negative
+    @pytest.mark.xfail(reason="fixing this bug right now")  # маркировка теста, как ожидаемо падающего
     def test_registration2(self, browser):
-
         link = "http://suninjuly.github.io/registration2.html"
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        input2.send_keys("Ivanov")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        input3.send_keys("ivan@gmail.com")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assertEqual проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+    @pytest.mark.skip(reason="no way of currently testing this")  # маркировка пропуска теста с комментом
+    def test_registration3(self, browser):
+        link = "http://suninjuly.github.io/registration1.html"
         browser.get(link)
 
         # Ваш код, который заполняет обязательные поля
