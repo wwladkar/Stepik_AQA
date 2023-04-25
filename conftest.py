2e5e8522421299aad722fd8c23e9dcc0a376e4d2
wwladkar <wwladkar@gmail.com>
2023-04-25 07:10:13 +0300

    Moved files for directory tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(scope="function")  # декоратор фикстуры с областью видимости "function"
@@ -10,8 +12,21 @@ def browser():
     print("\nstart browser for test..")
     options = Options()
     options.add_argument("start-maximized")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     yield browser
     # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
+
+
+@pytest.fixture(scope="function")  # декоратор фикстуры с областью видимости "function"
+def driver():
+    print("\nstart driver for test..")
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    yield driver
+    # этот код выполнится после завершения теста
+    print("\nquit driver..")
+    driver.quit()
+
+
+
---
+++ tests/mod3_les6_step3_pytest_parametrize_test.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+languages = [
+    ("ru", "русский"),
+    ("de", "немецкий"),
+    pytest.param("ua", "украинский", marks=pytest.mark.xfail(reason="no ua language")),
+    ("en-gb", "английский")
+]
+
+
+@pytest.mark.parametrize("code, lang", languages)
+def test_guest_should_see_login_link_in_chrome(browser, code, lang):
+    link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
+    print("Проверяемый язык %s" % lang)
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+@pytest.mark.parametrize("code, lang", languages)
+def test_guest_should_see_login_link_in_firefox(driver, code, lang):
+    link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
+    print("Проверяемый язык %s" % lang)
+    driver.get(link)
+    driver.find_element(By.CSS_SELECTOR, "#login_link")
